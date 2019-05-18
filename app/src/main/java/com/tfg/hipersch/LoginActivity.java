2997114362146c0fd09f8e1037f79fef2f8729a9
marioanloru <marioanloru@gmail.com>
2019-05-18 14:42:30 +0200

    First commit

---
+++ app/src/main/java/com/tfg/hipersch/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.tfg.hipersch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_login);
+    }
+}
